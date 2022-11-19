67420353dd67d80d5ca47bd4531d46a69119bfdf
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-11-19 16:50:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+//TO REMOVE AN ELEMENT
+//1. Select the element
+let mainElement = document.querySelector('main#main');
+
+//2.Delete element
+mainElement.remove()
+
+//TO ADD A NEW ELEMENT
+//1.create new element
+let newHeader = document.createElement('h1');
+
+//2. get access to parent element that will hold it
+ let parentElement = document.body;
+
+ //3. append
+
+
+
+ newHeader.id ='victory';
+ newHeader.innerHTML = "YOUR-NAME is the champion";
+ newHeader;
+
